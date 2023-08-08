1e8319e69de30133e23f43304e9734c570eb3c52
Vitalii <vitalii.hryshko.rss@gmail.com>
2023-08-08 23:16:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = [];
+    let j = 0;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    while (j < arr.length) {
+        if (str.includes(arr[j])) {
+            str = str.replace(arr[j], '');
+            j = 0;
+        } else {
+            j++;
+        }
+    }
+
+    return str === '';
 }
